73b7fc819539d57bf73667a899d5bdcf561bd667
Dawid <prostreet950@gmail.com>
2017-12-06 13:26:42 +0100

    First version

---
+++ matrix.h
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+class CMatrix{
+  struct object;
+  object* data;
+
+};
+
+struct CMatrix::object 
+{
+  float **m;
+  unsigned int n;
+  unsigned int col;
+  unsigned int row;
+
+
+  object(unsigned int ncol, unsigned int nrow) 
+  {
+  unsigned int i=0;
+  n=1;
+  col=ncol;
+  row=nrow;
+  m=new float *[col];
+  for(i=0;i<col;i++)
+    m[i]=new float[row];
+  };
+
+
+  ~object()
+  {
+  unsigned int i;
+  for(i=0;i<col;i++)
+    delete [] m[i];
+  delete [] m;
+  };
+
+  object* detach()
+  {
+    if(n==1)
+      return this;
+    object* t=new object(col, row);
+    copy(t->m);
+    n--;
+    return t;
+  };
+
+  void fill(float var, float ovar) 
+  {
+    unsigned int i;
+    unsigned int j;
+    for(i=0;i<col;i++)
+      for(j=0;j<row;j++)
+        if(i==j)
+          m[i][j]=var;
+        else
+          m[i][j]=ovar;
+  }
+
+  void copy(float **p)
+  {
+    unsigned int i;
+    unsigned int j;
+    for(i=0;i<col;i++)
+      for(j=0;j<row;j++)
+        p[i][j]=m[i][j];
+  }
+};
